dc90c057071c2947923141a7228a41811b32a895
Info Aakvatech <info@aakvatech.com>
2020-02-28 00:41:09 +0000

    Update: overtime_hook

--- payware/payware/overtime_hook.py
+++ payware/payware/overtime_hook.py
@@ -3,36 +3,38 @@ import json
 import frappe
 from frappe.model.document import Document
 from dateutil.relativedelta import relativedelta
-from frappe.utils import cint, format_datetime,get_datetime_str,now_datetime,add_days,today,formatdate,date_diff,getdate,add_months,flt, nowdate, fmt_money, add_to_date, DATE_FORMAT, rounded, to_timedelta, get_time, get_datetime,time_diff
+from frappe.utils import get_first_day, getdate, flt, add_to_date, rounded, time_diff_in_hours ,get_first_day_of_week
 from frappe import _
 from erpnext.hr.doctype.employee.employee import is_holiday
 from calendar import monthrange
-from datetime import datetime
-from dateutil.parser import parse
 from payware.payware.doctype.payware_settings import payware_settings
 from frappe.desk.form.linked_with import get_linked_docs, get_linked_doctypes
 
 
 
 
-def validate_daily_overtime(emp_name, overtime):
-	pass
+def validate_daily_overtime(overtime):
+    if overtime <= payware_settings.get_max_daily():
+        return overtime
+    else:
+        return payware_settings.get_max_daily()
 
 
 def validate_weekly_overtime(emp_name, overtime,date):
-	pass
+	start_week_date = get_first_day_of_week(date)
 
 
 def validate_monthly_overtime(emp_name, overtime,date):
-	pass
+	start_month_date = get_first_day(date)
 
 	
 def validate_maximum_overtime_for_employee(emp_name, overtime,date):
 	return True
 
 
-def time_diff_in_hours(start, end):
+def time_diff_in_hour(start, end):
 	return round((end-start).total_seconds() / 3600, 1)
+
 
 def get_chekout_time(doctype,docname):
     linkinfo = get_linked_doctypes(doctype)
@@ -70,21 +72,23 @@ def calculate_overtime(doc, method):
     holiday = is_holiday(doc.employee, doc.attendance_date)
     start_time = frappe.db.get_value("Shift Type", doc.shift, "start_time")
     end_time = frappe.db.get_value("Shift Type", doc.shift, "end_time")
-    shift_duration = time_diff_in_hours(start_time, end_time)
+    shift_duration = time_diff_in_hour(start_time, end_time)
 
     if payware_settings.get_overtime_mode() == "Checkout Time":
-        
-        employee_checkin_date=getdate(get_chekin_time(doc.doctype,doc.name))
+        employee_checkout_time=get_chekout_time(doc.doctype,doc.name)
+        if not employee_checkout_time:
+            return
         employee_checkin_time=get_chekin_time(doc.doctype,doc.name)
+        if not employee_checkin_time:
+            return
+        employee_checkin_date=getdate(employee_checkin_time)
         supposed_employee_checkin_time = str(employee_checkin_date)+ " " + str(start_time)
         supposed_employee_checkout_time = add_to_date(date = supposed_employee_checkin_time, hours = shift_duration)
-        employee_checkout_time=get_chekout_time(doc.doctype,doc.name)
-        frappe.msgprint("supposed_employee_checkin_time" + " " + str(supposed_employee_checkin_time))
-        frappe.msgprint("employee_checkin_time" + " " + str(employee_checkin_time))
-        frappe.msgprint("employee_checkout_time" + " " + str(employee_checkout_time))
-        frappe.msgprint("supposed_employee_checkout_time" + " " + str(supposed_employee_checkout_time))
-        # overtime = flt(employee_checkin_time - supposed_employee_checkout_time, 2)
-        overtime = time_diff(employee_checkout_time, supposed_employee_checkout_time)
+
+        overtime = flt(time_diff_in_hours(employee_checkout_time, supposed_employee_checkout_time),2)
+
+        if overtime < 0:
+            return
 
         if not holiday:
             frappe.msgprint("overtime_normal" +" "+ str(overtime))
