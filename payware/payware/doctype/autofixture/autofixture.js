8d23089e4cb5075f0d487b7c0e2e4f3de959378f
av-dev2 <av-dev2@aakvatech.com>
2021-06-04 08:56:51 +0300

    changes

--- payware/payware/doctype/autofixture/autofixture.js
+++ payware/payware/doctype/autofixture/autofixture.js
@@ -2,7 +2,7 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('AutoFixture', {
-	refresh: function(frm) {
-		frm.trigger()
-	}
+	//refresh: function(frm) {
+		//frm.trigger()
+	//}
 });
