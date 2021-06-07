38d065c9f246a6d5ac0b5fef3cbe0248c628a9c8
av-dev2 <av-dev2@aakvatech.com>
2021-06-07 14:24:36 +0300

    Changes Made

--- payware/payware/doctype/autofixture/autofixture.py
+++ payware/payware/doctype/autofixture/autofixture.py
@@ -10,7 +10,7 @@ from frappe.utils import fixtures
 
 class AutoFixture(Document):
 	def validate(self):
-		fixture_lab = []
+		frappe.msgprint("Validate")
 		for identified_app in frappe.get_installed_apps():
 			for hook in frappe.get_hooks("fixtures", app_name=identified_app):
 				filters = None
@@ -18,22 +18,16 @@ class AutoFixture(Document):
 					doctype_name = hook.get("doctype")
 					filters = hook.get("filters")
 					for fil in filters:
-						for filter in fil[2]:
+						for filter in list(fil[2]):
 							fixture_doctype = filter.split("-")[0]
-							
-							for item in self.fixture:
-								row = fixture_lab.append({"doctype_name": item.doctype_name, "filter": item.filter, "identified_app": item.identified_app, "fixture_doctype": item.fixture_doctype})
-								row.doctype_name = doctype_name
-								row.filter = filter
-								row.identified_app = identified_app
-								row.fixture_doctype = doctype_name
-								row.save()
 
-"""
-autofixture = frappe.new_doc("AutoFixture")
-autofixture.old_doc = old_dc
-autofixture.filter = filter
-autofixture.app = app
-autofixture.new_doc = new_dc
-autofixture.save()
-"""
+							doc = frappe.new_doc("Fixtures")
+
+							doc.doctype_name = doctype_name
+							doc.filter = filter
+							doc.identified_app = identified_app
+							doc.fixture_doctype = fixture_doctype
+							doc.save()
+							doc.submit()
+
+	#print(validate())
